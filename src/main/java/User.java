49d516a0fcf520550d40a0b16251e4862b098d6e
qwerty64200 <2981695254@qq.com>
2018-03-09 16:19:22 +0800

    add a filed age and getter and setter on User

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,7 @@
 public class User {
     private Integer id;
     private String name;
+    private Integer age;
 
     public User() {
     }
@@ -19,5 +20,13 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
