8063efe8835ea7070e0a8abe2c9e057874748a5f
qwerty64200 <2981695254@qq.com>
2018-03-09 16:01:50 +0800

    add a filed name and getter and setter on User

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,6 @@
 public class User {
     private Integer id;
+    private String name;
 
     public User() {
     }
@@ -10,5 +11,13 @@ public class User {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
